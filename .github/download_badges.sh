51288af8b4fd59259c63778199d5f6521f6f2751
blackzero <itsnavee@gmail.com>
2024-12-02 23:42:55 +0000

    chore: update badge script to use .github/badges directory

--- .github/download_badges.sh
+++ .github/download_badges.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Create badges directory if it doesn't exist
-mkdir -p badges
+mkdir -p .github/badges
 
 # Download badges
-curl -o badges/python.svg https://img.shields.io/badge/python-v3.9+-blue.svg
-curl -o badges/status.svg https://img.shields.io/badge/status-active-success.svg
-curl -o badges/issues.svg https://img.shields.io/badge/issues-welcome-yellow.svg
-curl -o badges/contributions.svg https://img.shields.io/badge/contributions-welcome-orange.svg
-curl -o badges/license.svg https://img.shields.io/badge/license-MIT-blue.svg
+curl -o .github/badges/python.svg https://img.shields.io/badge/python-v3.9+-blue.svg
+curl -o .github/badges/status.svg https://img.shields.io/badge/status-active-success.svg
+curl -o .github/badges/issues.svg https://img.shields.io/badge/issues-welcome-yellow.svg
+curl -o .github/badges/contributions.svg https://img.shields.io/badge/contributions-welcome-orange.svg
+curl -o .github/badges/license.svg https://img.shields.io/badge/license-MIT-blue.svg
 
-echo "Badges downloaded successfully!" 
+echo "Badges downloaded successfully to .github/badges!" 
