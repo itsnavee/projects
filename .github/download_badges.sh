fd6efbf52d18ab9c0ceadfd18cdcd8ac357c1c6a
blackzero <itsnavee@gmail.com>
2024-12-03 00:09:23 +0000

    docs: update README layout and Python version in badges

--- .github/download_badges.sh
+++ .github/download_badges.sh
@@ -4,7 +4,7 @@
 mkdir -p .github/badges
 
 # Download badges
-curl -o .github/badges/python.svg https://img.shields.io/badge/python-v3.9+-blue.svg
+curl -o .github/badges/python.svg https://img.shields.io/badge/python-v3.11+-blue.svg
 curl -o .github/badges/status.svg https://img.shields.io/badge/status-active-success.svg
 curl -o .github/badges/issues.svg https://img.shields.io/badge/issues-welcome-yellow.svg
 curl -o .github/badges/contributions.svg https://img.shields.io/badge/contributions-welcome-orange.svg
